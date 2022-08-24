6ea45cc284a34a47df8bf87e4c60c43cd123e716
HFsdb <1905776345@qq.com>
2022-08-24 16:17:15 +0800

    git push -u origin master

--- client/src/main/java/com/gmc/client/ClientApplication.java
+++ client/src/main/java/com/gmc/client/ClientApplication.java
@@ -1,6 +1,7 @@
 package com.gmc.client;
 
 import com.gmc.server.ServerInterface;
+import com.gmc.server.loadbalance.hash.HashLoadBalance;
 import com.gmc.server.loadbalance.pooling.PoolingLoadBalance;
 import com.gmc.server.service.HelloService;
 import org.springframework.boot.SpringApplication;
@@ -12,7 +13,7 @@ public class ClientApplication {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(ClientApplication.class, args);
         new ServerInterface();
-        HelloService helloService = ServerInterface.getProxy(HelloService.class,"1.0",new PoolingLoadBalance(),3000L);
+        HelloService helloService = ServerInterface.getProxy(HelloService.class,"1.0",new HashLoadBalance(),3000L);
         System.out.println("name:" + helloService.hello("gmc"));
 
     }
